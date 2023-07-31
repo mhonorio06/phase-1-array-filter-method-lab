93cc7c1908ac96590cc0d5645b464fec76ef96a8
Manny <mhfernan35277@gmail.com>
2023-07-31 03:38:31 -0700

    complete array filter method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching (array, name) {
+    return array.filter(function (driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+}
+function fuzzyMatch (array, begName) {
+    let partOfName = begName.length;
+    return array.filter(function (driver) {
+        return driver.slice(0,partOfName) ===begName;    
+});
+}
+function matchName (array, name) {
+    return array.filter(function (driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
